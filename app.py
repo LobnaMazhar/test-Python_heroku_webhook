ffd55b4aa49a5b6d1b7c473da660b45d1efbba88
Lobna_Mazhar <LobnaMazhar96@gmail.com>
2017-04-17 12:55:57 +0200

    test insert into DB by param

--- app.py
+++ app.py
@@ -126,7 +126,7 @@ def requestDB(req):
     conn = connectDB()
    ### createTable_Answers(conn)
     ###createTable(conn)
-   ### insertIntoDB(conn, "Nany", 20)
+    insertIntoDB(conn, "XYZ", 20)
     
     print "before " + name
     name = selectDB(conn)
