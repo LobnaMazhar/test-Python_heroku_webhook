d6a6a5c9ec6eeffef3bf42c0133fa702be43a2cd
Lobna_Mazhar <LobnaMazhar96@gmail.com>
2017-04-28 13:14:18 +0200

    test menu

--- app.py
+++ app.py
@@ -240,7 +240,44 @@ def addMenu():
     access_token = "EAASr1ZCrcjQkBADfZCmEo87CLaDUTy9pDWWn8CZCX45ekEcHxbk459jAcGnyGENSZBbcNuSLgRGjToh3MXPUYeqZBlEwEtl3yVinBBFdxdssk1Ga2n7zTfKLMiiXsuU35H3KsPrISHmaDbsSZAoa6PQes8V2sqBRVJZAEYOqIZB5vwZDZD"
     url = "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=" + access_token
     values = {
-  "persistent_menu":[]
+  "persistent_menu":[{
+          "locale":"default",
+          "composer_input_disabled":False,
+          "call_to_actions":[
+              {
+                  "title":"My Account",
+                  "type":"nested",
+                  "call_to_actions":[
+                      {
+                          "title":"reqGame",
+                          "type":"postback",
+                          "payload":"PAYBILL_PAYLOAD"
+                          },
+                      {
+                          "title":"History",
+                          "type":"postback",
+                          "payload":"HISTORY_PAYLOAD"
+                          },
+                      {
+                          "title":"Contact Info",
+                          "type":"postback",
+                          "payload":"CONTACT_INFO_PAYLOAD"
+                          }
+                      ]
+                  },
+              {
+                  "type":"web_url",
+                  "title":"Latest News",
+                  "url":"http://petershats.parseapp.com/hat-news",
+                  "webview_height_ratio":"full"
+                  }
+              ]
+          },
+      {
+          "locale":"zh_CN",
+          "composer_input_disabled":False
+          }
+     ]
   }
 
     r = requests.post(url, data = values, headers={'Content-type': 'application/json'})
