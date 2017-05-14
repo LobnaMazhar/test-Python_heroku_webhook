fad20a498d16718152921c2e33700d57a36057cf
Lobna_Mazhar <LobnaMazhar96@gmail.com>
2017-05-14 22:41:32 +0200

    test getAppScopedID V2

--- app.py
+++ app.py
@@ -384,7 +384,8 @@ def getAppScopedID(req):
     r = requests.get(url)
     print(r.status_code, r.reason)
     print(r.text[:300] + '...')
-    print(r.get("from").get("id"))
+    res = json.loads(r.text)
+    print(res.get("from").get("id"))
     print "--------------------->>>>>>>>>>>>>>" + "<<<<<<<<<<<<--------------------"
     
 def makeWebhookResult(req):
