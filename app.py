087e2cd05f3026e2c59f76b101bb8b433f51e434
Lobna_Mazhar <LobnaMazhar96@gmail.com>
2017-07-08 20:18:13 +0200

    Notify with Gif V1.10

--- app.py
+++ app.py
@@ -402,9 +402,9 @@ def notifyWithGIF():
     paramUrl = {"url":attachedGif}
     paramPayload = json.dumps(paramUrl, ensure_ascii=False)
     
-    paramAttachment = { "type": "image", "payload": json.dumps(paramPayload, ensure_ascii=False) }
+    paramAttachment = { "type": "image" }
    # paramAttachment["type"] = "image"
-   # paramAttachment["payload"] = json.dumps(paramPayload, ensure_ascii=False)
+    paramAttachment["payload"] = json.dumps(paramPayload, ensure_ascii=False)
     
     paramRecipient = { "id": userID }
     paramMessage = { "attachment": json.dumps(paramAttachment, ensure_ascii=False) }
